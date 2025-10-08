5039e8cd957fb8f2848502ce6c53229ca1d4805a
Valeryste <valera.stelmashock@yandex.ru>
2025-10-08 17:38:57 +0300

    change styles for register page

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -7,34 +7,40 @@ require_once '../layout/header.php';
     <form class="card" action="/actions/auth/register.php" method="post">
         <h2>Регистрация</h2>
 
-        <label for="username">
-            Имя
-            <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder="Красавчик92"
-                    required
-                    value="<?php echo htmlspecialchars($_POST['username'] ?? ''); ?>"
-            >
-        </label>
-        <div class="error"><?php echo $_SESSION['errors']['username'] ?? ''; ?></div>
+        <div class="form-group">
+            <label for="username">
+                Имя
+                <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Красавчик92"
+                        class="form-control"
+                        required
+                        value="<?php echo htmlspecialchars($_POST['username'] ?? ''); ?>"
+                >
+            </label>
+            <div class="error"><?php echo $_SESSION['errors']['username'] ?? ''; ?></div>
+        </div>
 
-        <label for="password">
-            Пароль
-            <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="******"
-                    required
-            >
-        </label>
-        <div class="error"><?php echo $_SESSION['errors']['password'] ?? ''; ?></div>
+        <div class="form-group">
+            <label for="password">
+                Пароль
+                <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="******"
+                        class="form-control"
+                        required
+                >
+            </label>
+            <div class="error"><?php echo $_SESSION['errors']['password'] ?? ''; ?></div>
+        </div>
 
         <div class="error"><?php echo $_SESSION['errors']['message'] ?? ''; ?></div>
 
-        <button type="submit" id="submit">Продолжить</button>
+        <button type="submit" class="btn-primary" id="submit">Продолжить</button>
     </form>
 
     <div class="text-center">
